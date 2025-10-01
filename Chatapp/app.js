88db493c2524ff25c00fcd935680a424a9aa83ed
Alexmour0907 <mouritsenalexander@gmail.com>
2025-10-01 11:34:54 +0200

    henter ut meldinger til HTML og consoll

--- Chatapp/app.js
+++ Chatapp/app.js
@@ -10,16 +10,23 @@ const db = new Database('chat_db.db');
 //Port
 const PORT = 3000;
 
+//console log lenken til siden på localhost
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+  
+//Serve statiske filer fra "public"-mappen
+app.use(express.static('public'));
 
 //Ruter
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(__dirname + '/public/index.html');
 });
 
+
+//Bruk .get for å hente 1 menlding, bruk .all for å hente alle. 
+//Man kan velge rekkefølge med ORDER BY column_name ASC/DESC (ASC = ascending, DESC = descending)
 app.get('/hentMeldinger', (req, res) => {
-    const meldinger = db.prepare('SELECT * FROM meldinger').get();
+    const meldinger = db.prepare('SELECT * FROM meldinger').all();
     res.json(meldinger);
 });
